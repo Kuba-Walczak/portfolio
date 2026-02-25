9e0a41dcab98311966af110272d51bb56ce76a77
Jacob <kubawalczak005@gmail.com>
2026-02-25 17:40:07 +0100

    feat: added simple footer

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  const currentYear = new Date().getFullYear()
+
+  return (
+    <footer className="w-full py-4 border-t bg-white/5 backdrop-blur-sm">
+      <div className="mx-auto">
+        <p className="text-center text-xl text-muted-foreground">
+          © {currentYear} Kuba Walczak. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  )
+}
